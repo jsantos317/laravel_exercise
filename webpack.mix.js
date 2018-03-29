19566071784f2be62fd3adbe1fcbee29a2064cfa
Joseph Santos <jsantos317@gmail.com>
2018-03-29 15:53:12 -0500

    add Metronic template

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,19 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .scripts([
+        'resources/metronic/assets/vendors/base/vendors.bundle.js',
+        'resources/metronic/assets/demo/demo2/base/scripts.bundle.js',
+        'resources/metronic/assets/vendors/custom/fullcalendar/fullcalendar.bundle.js',
+        'resources/metronic/assets/app/js/dashboard.js'
+    ], 'public/js/metronic.js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .styles([
+        'resources/metronic/assets/vendors/base/vendors.bundle.css',
+        'resources/metronic/assets/demo/demo2/base/style.bundle.css'
+    ], 'public/css/metronic.css')
+    .copy('resources/metronic/assets/demo/demo2/media/img/logo/logo.png', 'public/images')
+    .copy('resources/metronic/assets/app/media/img/blog/blog1.jpg', 'public/images')
+    .copy('resources/metronic/assets/app/media/img/client-logos', 'public/images')
+    .copy('resources/metronic/assets/app/media/img/users', 'public/images')
+    .copyDirectory('resources/metronic/assets/vendors/base/fonts', 'public/css/fonts');
